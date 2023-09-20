206a3efdf4b3e05610128534d4f42e27a92be662
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-09-20 15:51:05 -0400

    First commit

---
+++ android/app/src/main/kotlin/count/app/app_countdown/MainActivity.kt
@@ -0,0 +1,6 @@
+package count.app.app_countdown
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Countdown App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime date = DateTime.now();
+
+  void countDown() {
+    setState(() {
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Escolha uma data para fazer a contagem regressiva',
+              style: TextStyle(fontSize: 25),
+              maxLines: 3,
+              softWrap: true,
+            ),
+            Text(
+              '${date.day}/${date.month}/${date.year}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                DateTime? newDate = await showDatePicker(
+                    context: context,
+                    initialDate: date,
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2027),
+                );
+                if(newDate == null) return;
+
+                setState(() => date = newDate);
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white12
+              ),
+              child: const Text('Select Date'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
