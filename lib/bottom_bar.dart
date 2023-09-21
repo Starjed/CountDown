573e6b8ccf6036792128bcfbb878623dcea5ca12
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-09-21 16:07:11 -0400

    Adiciona icones na bottom bar e modifica index dinamicamente

--- lib/bottom_bar.dart
+++ lib/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,27 +10,45 @@ class BottomBar  extends StatefulWidget {
 }
 
 class BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 1;
+
+  static final List<Widget> _widgetOptions = <Widget> [
+    const Text("Home"),
+    const Text("Countdown"),
+    const Text("Calendar")
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Countdown"),
       ),
-      body:  const Center(
-        child: Text(
-        "My body"
-        ),
+      body:  Center(
+        child: _widgetOptions[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         elevation: 10,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         selectedItemColor: Colors.blueGrey,
+        type: BottomNavigationBarType.fixed,
         unselectedItemColor: const Color(0xFF526480),
         items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.access_alarms_outlined), label: "Timer"),
-        BottomNavigationBarItem(icon: Icon(Icons.date_range), label: "Calendar")
+        BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_clock_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_clock_filled), label: "Countdown"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_calendar_date_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_calendar_date_filled), label: "Calendar"),
       ],),
     );
   }
